30eb158b8cc64f70f65d772ffb4af97e5b1e8cd7
boyan yankov <thinkaboutiter@gmail.com>
2020-12-11 02:50:57 +0200

    #3 add implementation

--- source/source.playground/Pages/2-counting-valleys.xcplaygroundpage/Contents.swift
+++ source/source.playground/Pages/2-counting-valleys.xcplaygroundpage/Contents.swift
@@ -1,3 +1,53 @@
 import Foundation
 
-var str = "Hello, playground"
+
+let path = "DDUUDDUDUUUD"
+
+enum Tracker {
+    static private(set) var valleys: Int = 0
+    static private(set) var mountains: Int = 0
+    static private(set) var denivelation: Int = 0 {
+        didSet {
+            if denivelation == 0 {
+                if oldValue < 0 {
+                    valleys += 1
+                }
+                else if oldValue > 0 {
+                    mountains += 1
+                }
+            }
+        }
+    }
+    
+    static func goUphill() {
+        denivelation += 1
+    }
+    
+    static func goDownhill() {
+        denivelation -= 1
+    }
+    
+    enum Direction {
+        static let uphill = "U"
+        static let downhill = "D"
+    }
+}
+
+
+func countingValleys(steps: Int, path: String) -> Int {
+    
+    for element in path {
+        let string = String(element)
+        if string == Tracker.Direction.uphill {
+            Tracker.goUphill()
+        }
+        else if string == Tracker.Direction.downhill {
+            Tracker.goDownhill()
+        }
+    }
+    
+    assert(Tracker.denivelation == 0, "Route should end at sea level (0)")
+    return Tracker.valleys
+}
+
+let result = countingValleys(steps: 8, path: path)
